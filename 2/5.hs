983f11ead0bb725407835761eeec853a6ca164af
a1arick <31973557+a1arick@users.noreply.github.com>
2019-03-25 15:31:22 +0300

    Update 5.hs
    remove main

--- 2/5.hs
+++ 2/5.hs
@@ -7,4 +7,3 @@ sumPair (head1:tail1) (head2:tail2) =  head1 + head2 : sumPair tail1 tail2
 sumLists :: [Integer] -> [Integer] -> [Integer] -> [Integer]
 sumLists x1 x2 x3 = sumPair x1 (sumPair x2 x3)
 
-main = print (sumLists  [1,2,3] [4,5] [6]) 
