68e477b5e1758e77ba70eb35f19540d01bb3b774
a1arick <31973557+a1arick@users.noreply.github.com>
2019-03-25 17:46:07 +0300

    Update 3.hs

--- 3/3.hs
+++ 3/3.hs
@@ -3,8 +3,8 @@ supFunc :: Integer -> Integer -> Integer -> [Integer] -> Integer
 sumTempAndNext :: [Integer] -> Integer
 maxSumIndex xs = supFunc 0 0 0 xs
 
-supFunc maxPos maxSum i xs | xs == [] = maxPos
-                        | sumTempAndNext xs <= maxSum = supFunc maxPos maxSum (i + 1) (tail xs)
-                        | otherwise = supFunc (i + 1) (sumTempAndNext xs) (i + 1) (tail xs)
+supFunc maxPos maxSum i xs | xs == []                    = maxPos
+                           | sumTempAndNext xs <= maxSum = supFunc maxPos maxSum (i + 1) (tail xs)
+                           | otherwise                   = supFunc (i + 1) (sumTempAndNext xs) (i + 1) (tail xs)
 
 sumTempAndNext xs = if length xs == 1 then head xs else head xs + head (tail xs)
