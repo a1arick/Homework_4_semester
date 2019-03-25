dd5785845b901ceb04520938ec5b1604ec1732af
a1arick <31973557+a1arick@users.noreply.github.com>
2019-03-25 17:46:43 +0300

    Update 2.hs

--- 3/2.hs
+++ 3/2.hs
@@ -2,6 +2,6 @@ func :: [Integer] -> [Integer]
 func l = filter (\x -> cont x) l
 
 cont :: Integer -> Bool
-cont x | x == 0 = True
+cont x | x == 0                                                 = True
        | x `mod` 10 == 1 || x `mod` 10 == 7 ||  x `mod` 10 == 9 = cont (x `div` 10) 
-       | otherwise = False 
+       | otherwise                                              = False 
